4f9e9d623e2bd57b9d40b0516e396d0c57cb4f30
Matthew Watkinson <matthewwatkinson@gmail.com>
2026-06-02 14:41:08 +0900

    json saving and loading added for basic list

--- app.py
+++ app.py
@@ -1,17 +1,38 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# next up: store the lists across sessions using JSON
+# next up: update json format so we have dictionaries with the name, the actual list dictionary
+# also: practice expander logic using last_list
 
 import streamlit as st
-from helper import *
+import json
 
-st.title("List title")
+last_list = {"red": False, "yellow": False, "green": True, "purple": False, "black": False}
 
-last_list = {"heels": False, "sandals": False, "sneakers": False, "clogs": False, "boots": False}
+# import json
+try:
+    with open("list_storage.json", "r") as file:
+        json_dictionary = json.load(file)
+except json.decoder.JSONDecodeError:
+    # Triggered if the file is completely empty or has invalid syntax
+    json_dictionary = {}
+except FileNotFoundError:
+    # Triggered if the file does not exist at all
+    json_dictionary = {}
+if "current" not in json_dictionary:
+    json_dictionary["current"] = {}
 
+#but only use json if we don't have a session state
 if "current_list" not in st.session_state:
-    st.session_state["current_list"] = last_list
+    st.session_state["current_list"] = json_dictionary["current"]
+    # need to initialise the true/false state for each item
+    for item in st.session_state["current_list"]:
+        st.session_state[f"cb_{item}"] = st.session_state["current_list"][item]
+else:
+    # otherwise we need to save the latest state to json
+    with open("list_storage.json", "w") as file:
+        json_dictionary["current"] = st.session_state["current_list"]
+        json.dump(json_dictionary, file, indent=2)
 
 if "add_button_input" not in st.session_state:
     st.session_state.add_button_input = False
@@ -22,14 +43,18 @@ if "mark_all_done" not in st.session_state:
 if "clear_all" not in st.session_state:
     st.session_state.clear_all = False
 
+
 current_list = st.session_state["current_list"]
-
+# sort by checked/unchecked and then name
 ordered_list = dict(sorted(current_list.items(), key=lambda item: (-item[1], item[0])))
+# reinsert into session state
+st.session_state["current_list"] = ordered_list
 
-def list_draw(target_list):
-        for item, state in target_list.items():
+def list_draw(target_dict_key: str):
+        target_dict = st.session_state[target_dict_key]
+        for item, state in target_dict.items():
             def update_state(name=item):
-                st.session_state["current_list"][name] = st.session_state[f"cb_{name}"]
+                st.session_state[target_dict_key][name] = st.session_state[f"cb_{name}"]
 
             @st.dialog(title="Delete this item?", dismissible=False)
             def delete_confirm(name=item):
@@ -44,12 +69,10 @@ def list_draw(target_list):
 
             def delete_update(name=item):
                 
-                del st.session_state["current_list"][name]
+                del st.session_state[target_dict_key][name]
                 del st.session_state[f"{item}_delete_button"]
             
             col1, col2 =st.columns([1, 1])
-            #list_container = st.container(horizontal=True)
-            #with list_container:
             with col1:
                 st.checkbox(
                     label=item,
@@ -63,6 +86,7 @@ def list_draw(target_list):
                     key=f"{item}_delete_button",
                     on_click=delete_confirm
                 )
+            
 
 control_button_container = st.container(horizontal=True)
 with control_button_container:
@@ -114,4 +138,4 @@ if st.session_state.add_button_input:
             st.rerun()  # Instantly refreshes to show updated state
 
    
-list_draw(ordered_list)
+list_draw("current_list")
